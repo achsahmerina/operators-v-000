d048c545a264a488a95908166060fabb60002ff8
Achsah Abraham <achsahmp@gmail.com>
2018-08-02 19:16:17 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,11 @@ end
 
 
 def not_safe?(speed)
-	speed < 40
-	if speed < 40 ? "false" : "true"
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	else speed > 40 && speed < 60
+		return false
 	end
 end
